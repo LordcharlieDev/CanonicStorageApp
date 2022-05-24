bec162413abeb9ce3901195b7fd9687bce6419db
Oleksandr <sasha2003demchuk@gmail.com>
2022-05-24 10:32:53 +0300

    Add AccountController, login and register models.

---
+++ CanonicStorageApp/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CanonicStorageApp.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Не введено Login")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Не введено пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ CanonicStorageApp/Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CanonicStorageApp.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Не введено Login")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Не введено пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль введено неправильно")]
+        public string ConfirmPassword { get; set; }
+    }
+}
