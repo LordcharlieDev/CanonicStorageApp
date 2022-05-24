fd60421c7cd20ffc893fb525a76c574984649b0b
Oleksandr <sasha2003demchuk@gmail.com>
2022-05-24 10:21:39 +0300

    Add project files.

---
+++ CanonicStorageApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CanonicStorageApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
