aaca0ffe012e04fcbc3e403fa503f164303c2d52
Oleksandr <sasha2003demchuk@gmail.com>
2022-06-01 21:02:23 +0300

    Add check user and show users button if current ones are admin.

--- CanonicStorageApp/Controllers/HomeController.cs
+++ CanonicStorageApp/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using CanonicStorageApp.Models;
+using CNNCStorageDB.Data;
+using CNNCStorageDB.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CanonicStorageApp.Controllers
@@ -8,15 +11,18 @@ namespace CanonicStorageApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private CNNCDbContext _context = new CNNCDbContext();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            User user = new User();
+            user.IsAdmin = await _context.Users.Where(u => u.Login == User.Identity.Name).Select(x => x.IsAdmin).FirstOrDefaultAsync();
+            return View(user);
         }
 
         public IActionResult Privacy()
